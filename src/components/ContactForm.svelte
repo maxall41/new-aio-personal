4b55b6b39d698ad6046afc30fd977e7a5318e3a9
maxall4 <maxall4@protonmail.com>
2023-03-28 11:45:58 -0700

    A

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -1,26 +1,38 @@
 <script>
   import { form, field } from 'svelte-forms';
   import { required, email } from 'svelte-forms/validators';
+  import { API_URL } from '@/data/constants';
 
   const emailField = field('email', '', [required(),email()]);
   const messageField = field('message', '', [required()]);
   const contactForm = form(emailField,messageField);
 
+  let token = ""
+
+  function turnstileComplete(data) {
+console.log(data);
+token = data
+  }
+
   async function submit() {
+    console.log(token)
     const contactRequest = await fetch(API_URL + "api/contact/submit", {
 				method: "POST",
 				body: JSON.stringify({
 					email: emailField.value,
-          message: messageField.value
+          message: messageField.value,
+          turnstile_token: token
 				}),
 			});
-      console.log(contactRequest)
+      console.log(contactRequest);
   }
+
 </script>
 
-
 <section>
-  <div class="mb-4">
+     <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+  <section  class="mb-4">
+    <div class="mb-4">
       <label class="block text-textColor  text-sm font-bold mb-2" for="email">
         Email:
       </label>
@@ -30,9 +42,9 @@
       <label class="block text-textColor  text-sm font-bold mb-2" for="message" >
         Message:
       </label>
-     <input class="input-primary pb-32 text-start" id="message" type="text" placeholder="Hello, World!"  bind:value={$messageField.value} />
-    </div>
+     <input class="input-primary pb-32 text-start mb-4" id="message" type="text" placeholder="Hello, World!"  bind:value={$messageField.value} />
+     <div class="cf-turnstile" data-sitekey="0x4AAAAAAADj_D0FYcG1g4BO" data-callback="turnstileComplete"></div>
+    <button on:click={submit} class="relative h-9 w-full rounded-md bg-zinc-200 p-2 ring-zinc-400 transition-all hover:ring-2 mt-6 dark:bg-zinc-700" disabled={!$contactForm.valid}>Submit</button>
+    </section>
 
-<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
-  <button on:click={submit} class="relative h-9 w-full rounded-md bg-zinc-200 p-2 ring-zinc-400 transition-all hover:ring-2 dark:bg-zinc-700" disabled={!$contactForm.valid}>Submit</button>
 </section>
