1ebf4db4f9a5fbb648b933bdcb7fe296f0292530
maxall4 <maxall4@protonmail.com>
2023-03-28 01:33:02 -0700

    Added Spotify social link and updated about me

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -21,6 +21,7 @@ export const SOCIAL_LINKS = {
 	github: "https://github.com/maxall41",
 	soundcloud: "https://on.soundcloud.com/t7buB",
 	orcid: "https://orcid.org/0000-0002-0959-1164",
+	spotify: "https://open.spotify.com/artist/2ZS0LOJVCys33zUnbn66On",
 };
 
 export const API_URL = "https://blog-clap-service.vercel.app/";
